2669aeb0ec34d359be63e3145932cb6fbc06cce7
Volodymyr Hil <vhi@cabanabasecamp.local>
2016-05-17 13:12:48 +0200

    add more localized validation attributes

---
+++ Sources/Helpfulcore.Localization/Validation/CompareLocalizedAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Helpfulcore.Localization.Validation
+{
+    public class CompareLocalizedAttribute : CompareAttribute
+    {
+        public CompareLocalizedAttribute(string otherProperty) : base(otherProperty)
+        {
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return LocalizationFactory.LocalizationService.Localize(this.ErrorMessageResourceName, this.ErrorMessage);
+        }
+    }
+}
---
+++ Sources/Helpfulcore.Localization/Validation/RangeLocalizedAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helpfulcore.Localization.Validation
+{
+    public class RangeLocalizedAttribute : RangeAttribute
+    {
+        public RangeLocalizedAttribute(int minimum, int maximum) : base(minimum, maximum)
+        {
+        }
+
+        public RangeLocalizedAttribute(double minimum, double maximum) : base(minimum, maximum)
+        {
+        }
+
+        public RangeLocalizedAttribute(Type type, string minimum, string maximum) : base(type, minimum, maximum)
+        {
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return LocalizationFactory.LocalizationService.Localize(this.ErrorMessageResourceName, this.ErrorMessage);
+        }
+    }
+}
